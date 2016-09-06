59736c0ab3eaabd22c7d562c64d6c85a73f4deaf
fazhao <448517683@qq.com>
2016-09-06 15:45:07 +0800

    一些面试陷阱题目

---
+++ src/basic/initTest.java
@@ -0,0 +1,72 @@
+package basic;
+
+/**
+ * Created by fazhao on 16/9/6.
+ */
+public class initTest {
+    public static int x;
+    public void testDemo(){
+        int i ,j = 0 ,n = 0;
+        i = 100;
+        while(i>0){
+            /**
+            * 如果不初始化j则报错
+            * */
+            j = j+1;
+            j = i*2;
+            n = n +1;
+            i--;
+        }
+    }
+
+    /**
+    * 结果是0，陷阱！
+     * 因为x是形参，这里的x++是对形参进行加的，而不是全局的x
+     * 稍微修改一下就是1
+    * */
+    public static void name(int x ){
+        x++;
+        System.out.println("x:"+x);
+    }
+    /**
+    * 第一种
+    * */
+    public static void name1(int x ){
+        /**
+        * this can't be referenced from a static context
+        * */
+//        this.x++;
+        initTest.x++;
+    }
+    /**
+    * 第二种
+    * */
+    public static void name2(int y ){
+        x++;
+    }
+
+    public static void main(String args[]){
+//        name(x);
+//        name1(x);
+        name2(x);
+        System.out.println(x);
+        }
+}
+class Parent{
+    protected String name;
+    /**
+    * 不能获得private的权限
+    * */
+//    String name;
+    public Parent(){
+
+    }
+}
+class Child extends Parent{
+    public String getValue(){
+        return name;
+    }
+    public static void main(String args[]){
+        Parent p = new Parent();
+        }
+}
