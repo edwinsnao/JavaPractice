f8abeb5731fdc8510e2cb4ccb8617274ad1568fb
‘u013571833’ <448517683@qq.com>
2016-09-15 11:24:30 +0800

    enahcne IP

--- src/network/basic/IPDemo.java
+++ src/network/basic/IPDemo.java
@@ -1,11 +1,29 @@
 package network.basic;
 import java.net.*;
+import java.util.Enumeration;
+
 
 public class IPDemo {
 
     public static void main(String[] args) {
-
         try {
+            /**
+            *里面的参数是根据ifconfig里面的来决定的，
+             * 所以mac里面不是eth0而是en0，如果使用eth0则nullpointer
+            * */
+//            NetworkInterface eth0 = NetworkInterface.getByName("eth0");
+            NetworkInterface eth0 = NetworkInterface.getByName("en0");
+            Enumeration address1 = eth0.getInetAddresses();
+            while(address1.hasMoreElements())
+                System.out.println(address1.nextElement());
+            System.out.println(eth0.getDisplayName());
+
+            NetworkInterface lo0 = NetworkInterface.getByName("lo0");
+            Enumeration address2 = lo0.getInetAddresses();
+            while(address2.hasMoreElements())
+                System.out.println(address2.nextElement());
+            System.out.println(lo0.getDisplayName());
+
             /**
             * /127.0.0.1 is loopback address.
              /127.0.0.1 is a global address.
@@ -61,6 +79,8 @@ public class IPDemo {
         }
         catch (UnknownHostException ex) {
             System.err.println("Could not resolve " + args[0]);
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
 
     }
