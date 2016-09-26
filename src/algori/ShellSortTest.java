9f2bafc75eee602dc25a285de284bd4b5eae173f
‘u013571833’ <448517683@qq.com>
2016-09-26 23:05:04 +0800

    enhance

--- src/algori/ShellSortTest.java
+++ src/algori/ShellSortTest.java
@@ -5,6 +5,35 @@ package algori;
  */
 class ShellSortTest
 {
+    public void shell_sort1(int[] a){
+        int j = 0;
+        for (int gap = a.length/2; gap>0; gap/=2) {
+            for (int i = gap; i <a.length; i++) {
+                int tmp = a[i];
+                for (j = i-gap; j >=0 && tmp<a[j]; j-=gap) {
+                    a[j+gap] = a[j];
+                }
+                a[j+gap] = tmp;
+            }
+        }
+        snp(a);
+    }
+
+    public void shell_sort2(int[] a){
+        int j = 0;
+        int k = 0;
+        for (int gap = a.length/2; gap >0 ; gap/=2) {
+            for (int i = gap; i < a.length; i++) {
+                int tmp = a[i];
+                for (k = i-gap; k >=0 && tmp < a[k] ; k-=gap) {
+                    a[k+gap] = a[k];
+                }
+                a[k+gap] = tmp;
+            }
+        }
+        snp(a);
+    }
+    
     public void shell_sort(int [] arrays){
         for(int d=5;d>0;d=d-2){
             /**
@@ -42,7 +71,9 @@ class ShellSortTest
     {
         ShellSortTest s=new ShellSortTest();
         int[] a={45,20,80,40,26,58,66,70};
-        s.shell_sort(a);
+//        s.shell_sort(a);
+//        s.shell_sort1(a);
+        s.shell_sort2(a);
 
     }
 }
