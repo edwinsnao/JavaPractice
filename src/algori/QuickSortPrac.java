dfdb7f8b78b1b98973c2725e3ff6f7ad4e89093a
‘u013571833’ <448517683@qq.com>
2016-09-26 22:19:02 +0800

    enhance

--- src/algori/QuickSortPrac.java
+++ src/algori/QuickSortPrac.java
@@ -4,6 +4,32 @@ package algori;
  * Created by fazhao on 16/9/19.
  */
 public class QuickSortPrac {
+    public static void quick(int[] a,int i, int j){
+        if(i>j)
+            return;
+        int l = i;
+        int r = j;
+        int x = a[l];
+        while(l<r){
+            while(l<r && a[r]>x)
+                r--;
+            if(l<r)
+                a[l++] = a[r];
+            while(l<r && a[l]<x)
+                l++;
+            if(l<r)
+                a[r--] = a[l];
+        }
+        a[l] = x;
+        sort(a,i,l-1);
+        /**
+        *wrong
+        * */
+//        sort(a,l+1,r);
+        sort(a,r+1,j);
+
+    }
+
     public static void sort(int[] arr,int i,int j){
         if(i>j)
             return;
@@ -22,11 +48,12 @@ public class QuickSortPrac {
         }
         arr[l] = x;
         sort(arr,i,l-1);
-        sort(arr,l+1,r);
+        sort(arr,r+1,j);
     }
     public static void main(String args[]){
         int[] a={45,20,80,40,26,58,66,70};
-        sort(a,0,a.length-1);
+//        sort(a,0,a.length-1);
+        quick(a,0,a.length-1);
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+" ");
         }
