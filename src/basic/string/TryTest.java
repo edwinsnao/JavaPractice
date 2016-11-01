9bbec024284c1a225928e4156585749b0757991a
‘u013571833’ <448517683@qq.com>
2016-11-01 22:53:18 +0800

    enhance

--- src/basic/string/TryTest.java
+++ src/basic/string/TryTest.java
@@ -5,6 +5,8 @@ package basic.string;
  */
 public class TryTest {
     public static void main(String args[]){
+        String s = null;
+        String[] s1 = null;
         try {
             /**
             * 需要加上包名
@@ -13,9 +15,28 @@ public class TryTest {
             /**
             * cannot be cast to java.lang.Class
             * */
-            Class class2 = (Class) class1.newInstance();
+//            Class class2 = (class) class1.newInstance();
+            TryTest class2 = TryTest.class.newInstance();
             System.out.println(""+class1.getName());
-            System.out.println(""+class2.getName());
+            /**
+            * wrong
+            * */
+//            System.out.println(""+class2.main(s1));
+            System.out.println(""+class2);
+            /**
+            * 这样写也是可以的
+             * 而且最好写成"Java".equals(s);
+             * 这样就不需要写成
+             * if(s != null)
+             * s.equals("Java");
+             * 提高效率
+            * */
+
+            /**
+            * 编译通过，运行错误，NullPointerException
+             * equals里面不可以放null
+            * */
+            System.out.println(""+s.equals(null));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
