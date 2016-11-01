0786053e051387c17d58ec4aabf2e033dc569aec
‘u013571833’ <448517683@qq.com>
2016-11-01 22:39:36 +0800

    enhance

--- src/basic/string/TryTest.java
+++ src/basic/string/TryTest.java
@@ -5,6 +5,25 @@ package basic.string;
  */
 public class TryTest {
     public static void main(String args[]){
+        try {
+            /**
+            * 需要加上包名
+            * */
+            Class class1 = Class.forName("basic.string.TryTest");
+            /**
+            * cannot be cast to java.lang.Class
+            * */
+            Class class2 = (Class) class1.newInstance();
+            System.out.println(""+class1.getName());
+            System.out.println(""+class2.getName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         /**
         * 可以不catch，直接finally
         * */
