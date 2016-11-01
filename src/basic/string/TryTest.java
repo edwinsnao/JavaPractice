53877c05247d988e1701d669ec2981f5a7c3f86c
‘u013571833’ <448517683@qq.com>
2016-11-01 23:08:57 +0800

    enhance

--- src/basic/string/TryTest.java
+++ src/basic/string/TryTest.java
@@ -9,6 +9,12 @@ public class TryTest {
      * 这时Class.newInstance只找无参数构造函数
      * 因为找不到，所以报错
      * iationException: basic.string.TryTest
+     *
+     * 此外
+     * 下面B继承
+     * 如果只定义了有参构造函数，没有定义无参，编译器不会自动加上
+     * 这时super()只找无参数构造函数，但找不到，所以编译阶段就报错
+     * 所以需要supe(null)才可以通过
     * */
 //    TryTest(){
 //
@@ -29,15 +35,15 @@ public class TryTest {
             * cannot be cast to java.lang.Class
             * */
 //            Class class2 = (class) class1.newInstance();
-            Object class3 = class1.newInstance();
-            Object class4 = class1.newInstance();
+//            Object class3 = class1.newInstance();
+//            Object class4 = class1.newInstance();
             /**
             * newInstance不可以有参数
             * */
 //            Object class4 = class1.newInstance("hello");
-            TryTest class6 = (TryTest) class1.newInstance();
-            TryTest class2 = TryTest.class.newInstance();
-            TryTest class5 = TryTest.class.newInstance();
+//            TryTest class6 = (TryTest) class1.newInstance();
+//            TryTest class2 = TryTest.class.newInstance();
+//            TryTest class5 = TryTest.class.newInstance();
             System.out.println(""+class1.getName());
             /**
             * wrong
@@ -51,11 +57,11 @@ public class TryTest {
              basic.string.TryTest@5e2de80c
              basic.string.TryTest@1d44bcfa
             * */
-            System.out.println(""+class2);
-            System.out.println(""+class3);
-            System.out.println(""+class4);
-            System.out.println(""+class5);
-            System.out.println(""+class6);
+//            System.out.println(""+class2);
+//            System.out.println(""+class3);
+//            System.out.println(""+class4);
+//            System.out.println(""+class5);
+//            System.out.println(""+class6);
             /**
             * 这样写也是可以的
              * 而且最好写成"Java".equals(s);
@@ -73,10 +79,10 @@ public class TryTest {
 //            System.out.println(""+s.equals("hello"));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
         }
 
         /**
@@ -91,6 +97,7 @@ public class TryTest {
 
         class B extends TryTest{
             B(String s){
+//                super();
                 super(null);
             }
         }
@@ -98,5 +105,6 @@ public class TryTest {
 class B extends TryTest{
     B(String s){
         super(null);
+//        super();
     }
 }
