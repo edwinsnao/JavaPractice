9c4f542981069e2d86ca554e36bb64c0ec4fff08
‘u013571833’ <448517683@qq.com>
2016-11-09 11:17:28 +0800

    enhance

--- src/basic/ChangTest.java
+++ src/basic/ChangTest.java
@@ -6,6 +6,8 @@ package basic;
 public class ChangTest {
     static String str1 = new String("abc");
     static String str = new String("abc");
+    static StringBuilder strb1 = new StringBuilder("abc");
+    static StringBuilder strb = new StringBuilder("abc");
     public static void main(String[] args) {
 
         String str2 = "abc";
@@ -13,6 +15,7 @@ public class ChangTest {
         char[] ch = {'a', 'b', 'c'};
 
         change(str, ch);
+        change(strb, strb1);
 
         System.out.print(str + " ");
 
@@ -20,6 +23,8 @@ public class ChangTest {
 
         System.out.println(str1);
         System.out.println(str2);
+        System.out.println(strb1);
+        System.out.println(strb);
 
     }
         public static void change (String str,char[] ch){
@@ -29,5 +34,12 @@ public class ChangTest {
 //            System.out.println(str1);
             System.out.println(str);
             ch[0] = 'g';
+        }
+        public static void change (StringBuilder str,StringBuilder str1){
+//            str1 = "gbc";
+            str = new StringBuilder("gbc");
+            str1 = new StringBuilder("agbc");
+//            str1 = "hello";
+//            System.out.println(str1);
         }
 }
